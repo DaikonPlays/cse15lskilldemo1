3905936456ecea53fd914657a527978fae293318
DaikonPlays <kevinyan904@gmail.com>
2022-05-01 22:20:18 -0700

    test files

--- skilldemo.java
+++ skilldemo.java
@@ -1,1 +1,5 @@
-
+public class skilldemo{
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
+}
---
+++ skilldemotester.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class skilldemotester {
+    @Test
+    public void testMult() {
+        assertEquals(5, skilldemo.multiply(2, 3));
+    }
+}
